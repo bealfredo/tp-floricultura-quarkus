56dbb6ff2784b8dfafdceaf92137045d21374d61
aguiaralfredo <alfred00a@outlook.com>
2023-11-11 00:05:21 -0300

    Keys e perfil/tipousuario

---
+++ src/main/java/br/unitins/topicos1/floricultura/model/converterjpa/PerfilConverter.java
@@ -0,0 +1,21 @@
+package br.unitins.topicos1.model.converterjpa;
+
+import br.unitins.topicos1.model.Perfil;
+import jakarta.persistence.AttributeConverter;
+import jakarta.persistence.Converter;
+
+@Converter(autoApply = true)  // todo lugar que ele encontrar um perfil vai aplicar isso
+public class PerfilConverter implements AttributeConverter<Perfil, Integer> {
+
+  @Override
+  public Integer convertToDatabaseColumn(Perfil perfil) {
+    return (perfil == null ? null : perfil.getId());
+  }
+
+  @Override
+  public Perfil convertToEntityAttribute(Integer id) {
+    return Perfil.valueOf(id);
+  }
+  
+}
+
