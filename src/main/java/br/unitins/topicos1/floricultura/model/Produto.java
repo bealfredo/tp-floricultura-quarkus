7cb3e02d357a2c0c3e92bfea901d34c55134d61b
aguiaralfredo <alfred00a@outlook.com>
2023-11-10 23:48:09 -0300

    Primeiro - produtos

--- src/main/java/br/unitins/topicos1/floricultura/model/Produto.java
+++ src/main/java/br/unitins/topicos1/floricultura/model/Produto.java
@@ -1,17 +1,40 @@
 package br.unitins.topicos1.floricultura.model;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 
 @Entity
-public class TipoProduto extends DefaultEntity {
+public class Produto extends DefaultEntity {
 
     @Column (nullable = false)
     private String nome;
 
     private String descricao;
+
+    private String codigo;
+    
+    private String imagem;
+    
+    private Double precoVenda;
+
+    private double precoCusto;
+    
+    private Double desconto;
+    
+    private Integer quantidadeDisponivel;
+    
+    private LocalDateTime dataDisponivel;
+
+    @OneToMany(mappedBy = "tipoProduto")
+    private List<TipoProduto> tipoProduto;
+
+    
 
     @ManyToOne
     @JoinColumn(name = "id_categoriaproduto")
