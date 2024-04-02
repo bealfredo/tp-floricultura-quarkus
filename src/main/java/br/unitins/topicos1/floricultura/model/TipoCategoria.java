59a1fca65c95d475134c112a9c1343b4e2fc42a6
aguiaralfredo <alfred00a@outlook.com>
2024-04-02 15:31:35 -0300

    feat - TipoCategoria

---
+++ src/main/java/br/unitins/topicos1/floricultura/model/TipoCategoria.java
@@ -0,0 +1,42 @@
+package br.unitins.topicos1.floricultura.model;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+@JsonFormat(shape = JsonFormat.Shape.OBJECT)
+public enum TipoCategoria {
+
+  BIOLOGICA(1, "Biologica", "Categoria biológica para itens baseados em características biológicas."),
+  VIEW(2, "View", "Categoria de visualização para fins de exibição na aplicação.");
+
+  private final Integer id;
+  private final String label;
+  private final String description;
+
+  TipoCategoria(Integer id, String label, String description) {
+    this.id = id;
+    this.label = label;
+    this.description = description;
+  }
+
+  public Integer getId() {
+    return id;
+  }
+
+  public String getLabel() {
+    return label;
+  }
+
+  public String getDescription() {
+    return description;
+  }
+
+  public static TipoCategoria valueOf(Integer id) throws IllegalArgumentException {
+    if (id == null)
+      return null;
+    for (TipoCategoria item : TipoCategoria.values()) {
+      if (item.getId().equals(id))
+        return item;
+    }
+    throw new IllegalArgumentException("id inválido " + id);
+  }
+}
