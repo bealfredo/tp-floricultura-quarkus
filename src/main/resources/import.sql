0f507df83c41aa1e8c5bc5540b16dc79dea00461
aguiaralfredo <alfred00a@outlook.com>
2024-04-02 19:22:15 -0300

    ajust import

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -98,21 +98,6 @@ insert into itemvenda (preco, quantidade, id_produto, id_venda) values
 
 -- new with angular
 
--- @Entity
--- public class CategoriaPlanta extends DefaultEntity {
-
---     @Column(nullable = false)
---     private String nome;
-
---     @Column(length = 400)
---     private String descricao;
-
---     private Integer prioridade;
-
---     private Boolean ativa;
-
---     private TipoCategoria tipoCategoria;
-
 INSERT into categoriaplanta (nome, descricao, prioridade, ativa, tipocategoria) values
   ('Árvores', 'Esta categoria engloba uma variedade de árvores provenientes de diferentes espécies, proporcionando opções diversificadas para seu jardim ou espaço verde.', 1, true, 1),
   ('Flores', 'Esta categoria engloba uma variedade de flores para alegrar e decorar diferentes ambientes.', 2, false, 1),
