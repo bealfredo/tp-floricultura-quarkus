017a3c9e18b72abe5e35666def44cf8de7597b79
aguiaralfredo <alfred00a@outlook.com>
2024-05-15 11:41:04 -0300

    Ajuste telefone

--- src/main/java/br/unitins/topicos1/floricultura/dto/TelefoneDTO.java
+++ src/main/java/br/unitins/topicos1/floricultura/dto/TelefoneDTO.java
@@ -1,7 +1,11 @@
 package br.unitins.topicos1.floricultura.dto;
 
+import jakarta.validation.constraints.NotBlank;
+
 public record TelefoneDTO (
+    @NotBlank(message = "O ddd não pode ser nulo")
     String ddd,
+    @NotBlank(message = "O numero não pode ser nulo")
     String numero
 ) {
     
--- src/main/java/br/unitins/topicos1/floricultura/repository/TelefoneRepository.java
+++
@@ -1,9 +0,0 @@
-package br.unitins.topicos1.floricultura.repository;
-
-import br.unitins.topicos1.floricultura.model.Telefone;
-import io.quarkus.hibernate.orm.panache.PanacheRepository;
-import jakarta.enterprise.context.ApplicationScoped;
-
-@ApplicationScoped
-public class TelefoneRepository implements PanacheRepository<Telefone> {
-}
