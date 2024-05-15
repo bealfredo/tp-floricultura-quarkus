3757c6064a0fab089c473b385230de49e465e1af
aguiaralfredo <alfred00a@outlook.com>
2024-05-15 11:11:24 -0300

    ajustes 2 work

---
+++ src/main/java/br/unitins/topicos1/floricultura/model/Usuario.java
@@ -0,0 +1,105 @@
+package br.unitins.topicos1.floricultura.model;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.OneToOne;
+
+@Entity
+public class Usuario extends DefaultEntity {
+
+    private String nome;
+    private String sobreNome;
+    private String login;
+    private String cpf;
+    private String senha;
+    private LocalDate dataNascimento;
+    @OneToOne
+    private Telefone telefone;
+    // private TipoUsuario tipoUsuario;
+
+    // @ManyToMany(fetch = FetchType.EAGER)
+    // @JoinTable(
+    //     name = "usuario_endereco",
+    //     joinColumns = @JoinColumn(name = "id_usuario"),
+    //     inverseJoinColumns = @JoinColumn(name = "id_endereco")
+    // )
+    // private List<Endereco> listaEndereco;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+        public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    // public List<Endereco> getListaEndereco() {
+    //     return listaEndereco;
+    // }
+
+    // public void setListaEndereco(List<Endereco> listaEndereco) {
+    //     this.listaEndereco = listaEndereco;
+    // }
+
+    public String getSobreNome() {
+        return sobreNome;
+    }
+
+    public void setSobreNome(String sobreNome) {
+        this.sobreNome = sobreNome;
+    }
+
+    public LocalDate getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(LocalDate dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+    // public TipoUsuario getTipoUsuario() {
+    //     return tipoUsuario;
+    // }
+
+    // public void setTipoUsuario(TipoUsuario tipoUsuario) {
+    //     this.tipoUsuario = tipoUsuario;
+    // }
+
+    public Telefone getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(Telefone telefone) {
+        this.telefone = telefone;
+    }
+}
