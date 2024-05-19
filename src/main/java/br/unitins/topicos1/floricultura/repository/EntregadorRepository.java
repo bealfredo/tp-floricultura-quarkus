b59421c286e2faf3772be8732d9f1abbc85912df
aguiaralfredo <alfred00a@outlook.com>
2024-05-19 17:45:35 -0300

    feat entregador

---
+++ src/main/java/br/unitins/topicos1/floricultura/repository/EntregadorRepository.java
@@ -0,0 +1,11 @@
+package br.unitins.topicos1.floricultura.repository;
+
+import br.unitins.topicos1.floricultura.model.Entregador;
+import io.quarkus.hibernate.orm.panache.PanacheRepository;
+import jakarta.enterprise.context.ApplicationScoped;
+
+@ApplicationScoped
+public class EntregadorRepository implements PanacheRepository<Entregador>{
+  
+
+}
---
+++ src/main/java/br/unitins/topicos1/floricultura/resource/EntregadorResource.java
@@ -0,0 +1,86 @@
+package br.unitins.topicos1.floricultura.resource;
+import br.unitins.topicos1.floricultura.dto.EmailAvailableDTO;
+import br.unitins.topicos1.floricultura.dto.EmailAvailableResponseDTO;
+import br.unitins.topicos1.floricultura.dto.EntregadorCreateDTO;
+import br.unitins.topicos1.floricultura.dto.EntregadorResponseDTO;
+import br.unitins.topicos1.floricultura.dto.EntregadorUpdateDTO;
+import br.unitins.topicos1.floricultura.service.EntregadorService;
+import jakarta.inject.Inject;
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.DELETE;
+import jakarta.ws.rs.DefaultValue;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.PATCH;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.QueryParam;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+import jakarta.ws.rs.core.Response.Status;
+
+@Path("/entregadores")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
+public class EntregadorResource {
+
+    @Inject
+    EntregadorService service;
+
+    //check email availability
+    @POST
+    @Path("/emailavailable")
+    public Response checkEmailAvailable(EmailAvailableDTO dto) {
+        EmailAvailableResponseDTO retorno = service.checkEmailAvailable(dto);
+        return Response.status(200).entity(retorno).build();
+    }
+
+    @POST
+    public Response insert(EntregadorCreateDTO dto) {
+        EntregadorResponseDTO retorno = service.insert(dto);
+        return Response.status(Status.CREATED).entity(retorno).build();
+    }
+
+    @PATCH 
+    @Path("/{id}")
+    public Response update(EntregadorUpdateDTO dto, @PathParam("id") Long id) {
+        service.update(dto, id);
+        return Response.status(Status.NO_CONTENT).build();
+    }
+
+    @DELETE
+    @Path("/{id}")
+    public Response delete(@PathParam("id") Long id) {
+        service.delete(id);
+        return Response.status(Status.NO_CONTENT).build();
+    }
+
+    @GET
+    public Response findAll(
+        @QueryParam("page") @DefaultValue("0") int page,
+        @QueryParam("pageSize") @DefaultValue("100") int pageSize
+    ) {
+        return Response.ok(service.findByAll(page, pageSize)).build();
+    }
+
+
+    @GET
+    @Path("/{id}")
+    public Response findById(@PathParam("id") Long id) {
+        return Response.ok(service.findById(id)).build();
+    }
+    
+    // @GET
+    // @Path("/search/nome/{nome}")
+    // public Response findByNome(@PathParam("nome") String nome) {
+    //     return Response.ok(service.findByNome(nome)).build();
+    // }
+
+    // @GET
+    // @Path("/count")
+    // public long count(){
+    //     return service.count();
+    // }
+
+}
